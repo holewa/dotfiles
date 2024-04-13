8b141e8f1fe7677af6d165ec3512b82896772e25
Joel holewa <holewa@gmail.com>
2024-04-13 11:12:26 +0200

    Deleted code related to neorg.

--- dot_config/nvim/executable_init.lua
+++ dot_config/nvim/executable_init.lua
@@ -58,26 +58,6 @@ require("lazy").setup({
   require 'plugins.obsidian',
 
   {
-    "nvim-neorg/neorg",
-    build = ":Neorg sync-parsers",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("neorg").setup {
-        load = {
-          ["core.defaults"] = {}, -- Loads default behaviour
-          ["core.concealer"] = {}, -- Adds pretty icons to your documents
-          ["core.dirman"] = { -- Manages Neorg workspaces
-            config = {
-              workspaces = {
-                notes = "~/notes",
-              },
-            },
-          },
-        },
-      }
-    end,
-  },
-  {
     'nvim-java/nvim-java',
     dependencies = {
       'nvim-java/lua-async-await',
