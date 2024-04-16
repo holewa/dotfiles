d4095561d8f1a84080479a8335069971b209ad65
Joel holewa <holewa@gmail.com>
2024-04-16 14:16:50 +0200

    Added obsidian plugin.

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -30,6 +30,27 @@ require('lazy').setup({
   require 'plugins.lualine',
   require 'plugins.nvim-java',
   require 'plugins.harpoon',
+  require 'plugins.obsidian',
+
+  { -- This plugin
+    'Zeioth/compiler.nvim',
+    cmd = { 'CompilerOpen', 'CompilerToggleResults', 'CompilerRedo' },
+    dependencies = { 'stevearc/overseer.nvim' },
+    opts = {},
+  },
+  { -- The task runner we use
+    'stevearc/overseer.nvim',
+    commit = '68a2d344cea4a2e11acfb5690dc8ecd1a1ec0ce0',
+    cmd = { 'CompilerOpen', 'CompilerToggleResults', 'CompilerRedo' },
+    opts = {
+      task_list = {
+        direction = 'bottom',
+        min_height = 25,
+        max_height = 25,
+        default_detail = 1,
+      },
+    },
+  },
 
   {
     'Exafunction/codeium.nvim',
