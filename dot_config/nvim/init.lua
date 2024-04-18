aafd610bea8effd4d01de75a207603bd12a91e07
Joel holewa <holewa@gmail.com>
2024-04-18 11:34:44 +0200

    removed unused code.

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -101,12 +101,7 @@ require('lazy').setup({
 
   --keymaps from kickstarter
 
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
-  -- NOTE: Plugins can also be added by using a table,
-  -- with the first argument being the link and the following
-  -- keys can be used to configure plugin behavior/loading/etc.
   --
   -- Use `opts = {}` to force a plugin to be loaded.
   --
