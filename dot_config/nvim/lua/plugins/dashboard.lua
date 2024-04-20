7747e474d5c9804a75a8e6c94b3510c0b01d00ec
Joel holewa <holewa@gmail.com>
2024-04-20 12:25:42 +0200

    Dashboard config
    Added header and made a random quote generator class for dashboard

--- dot_config/nvim/lua/plugins/dashboard.lua
+++ dot_config/nvim/lua/plugins/dashboard.lua
@@ -1,38 +1,62 @@
+local quoteGenerator = require 'plugins.random-quote-generator'
+
+local findVimWikis = function()
+  require('telescope.builtin').find_files { cwd = '~/vimwiki/' }
+end
+
+-- Function to display a random quote in the footer
+local getRandomQuote = function()
+  local randomQuote = quoteGenerator.fetchRandomQuote()
+  local randomQuoteContentAndAuthor = { content = randomQuote.content, author = randomQuote.author }
+  return randomQuoteContentAndAuthor
+end
+
 return {
   'nvimdev/dashboard-nvim',
   event = 'VimEnter',
   config = function()
     require('dashboard').setup {
-      theme = 'hyper',
+      theme = 'doom',
       config = {
-        week_header = {
-          enable = true,
-        },
-        shortcut = {
-          { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+        -- header = require 'custom-headers.custom-header',
+        header = require 'custom-headers.garfield-header',
+        center = {
           {
-            icon = ' ',
-            icon_hl = '@variable',
-            desc = 'Files',
-            group = 'Label',
-            action = 'Telescope find_files',
+            icon = ' ',
+            icon_hl = 'Title',
+            desc = 'Find File           ',
+            desc_hl = 'String',
             key = 'f',
+            keymap = 'SPC f f',
+            key_hl = 'Number',
+            key_format = ' %s', -- remove default surrounding `[]`
+            action = 'Telescope find_files',
           },
           {
-            desc = ' Apps',
-            group = 'DiagnosticHint',
-            action = 'Telescope app',
-            key = 'a',
-          },
-          {
-            desc = ' dotfiles',
-            group = 'Number',
-            action = 'Telescope dotfiles',
+            icon = ' ',
+            desc = 'Find Dotfiles',
             key = 'd',
+            keymap = 'SPC f d',
+            key_format = ' %s', -- remove default surrounding `[]`
+            action = 'lua print(3)',
           },
+          {
+            icon = ' ',
+            icon_hl = 'Title',
+            desc = 'Find Notes           ',
+            desc_hl = 'String',
+            key = 'n',
+            keymap = 'SPC f f',
+            key_hl = 'Number',
+            key_format = ' %s', -- remove default surrounding `[]`
+            action = findVimWikis,
+          },
+        },
+        footer = {
+          getRandomQuote().content,
+          '- ' .. getRandomQuote().author,
         },
       },
     }
   end,
-  dependencies = { { 'nvim-tree/nvim-web-devicons' } },
 }
