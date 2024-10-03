88ae67c99219950a633aacd0166ab665e0afe26f
Joel Holewa <holewa@gmail.com>
2024-10-03 15:26:09 +0200

    Excluded patterns in neotree

--- dot_config/nvim/lua/plugins/executable_neo-tree.lua
+++ dot_config/nvim/lua/plugins/executable_neo-tree.lua
@@ -8,6 +8,27 @@ return {
   },
   config = function()
     require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          visible = false,    -- When true, they will be shown, hidden otherwise
+          hide_dotfiles = false, -- Hide dotfiles like .git, .env, etc.
+          hide_gitignored = false, -- Hide files ignored by git
+          hide_by_name = {
+            -- Add directories or files to ignore
+            "node_modules",
+            "target",
+            "build",
+            ".git",
+            "dist",
+            "__pycache__",
+          },
+          hide_by_pattern = { -- Use patterns to hide specific file types
+            "*.class",
+            "*.tmp",
+            ".*",
+          },
+        },
+      },
       git_status = {
         symbols = {
           -- Change type
