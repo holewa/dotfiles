d84f66465db7c44ed1ec928612ee7073e61915e7
Joel Holewa <holewa@gmail.com>
2024-11-04 12:52:20 +0100

    simpler script for ripgrep

--- run_once_install_ripgrep.sh
+++ run_once_install_ripgrep.sh
@@ -3,12 +3,7 @@
 # Check if rg (ripgrep) is installed
 if ! command -v rg &> /dev/null; then
     echo "Installing ripgrep (rg)..."
-    # Download the latest release of ripgrep binary
-    curl -LO https://github.com/BurntSushi/ripgrep/releases/download/$(curl -s https://api.github.com/repos/BurntSushi/ripgrep/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')/ripgrep_$(curl -s https://api.github.com/repos/BurntSushi/ripgrep/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")').deb
-    # Install the downloaded .deb package
-    sudo dpkg -i ripgrep_*.deb
-    # Clean up the downloaded .deb package
-    rm ripgrep_*.deb
+    sudo apt install ripgrep
 else
     echo "ripgrep (rg) is already installed."
 fi
