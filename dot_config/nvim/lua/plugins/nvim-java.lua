963cb9478db35f2c963c33920ad00735ae30c6bd
Joel Holewa <holewa@gmail.com>
2024-11-11 07:55:23 +0100

    Remove nvim-dap

--- dot_config/nvim/lua/plugins/nvim-java.lua
+++ dot_config/nvim/lua/plugins/nvim-java.lua
@@ -7,7 +7,7 @@ return {
     'nvim-java/nvim-java-dap',
     'MunifTanjim/nui.nvim',
     'neovim/nvim-lspconfig',
-    'mfussenegger/nvim-dap',
+    -- 'mfussenegger/nvim-dap',
     {
       'williamboman/mason.nvim',
       opts = {
