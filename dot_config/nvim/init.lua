9e6501ecf797f8ee6e7e4bf7415bea9cadb3cf50
Joel Holewa <holewa@gmail.com>
2024-11-13 18:18:13 +0100

    Fixed angular and new nvim path

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -483,14 +483,14 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
         -- tsserver = {},
-        angularls = {
-          cmd = ngls_cmd,
-          on_new_config = function(new_config)
-            new_config.cmd = ngls_cmd
-          end,
-          on_attach = on_attach,
-          capabilities = capabilities,
-        },
+        -- angularls = {
+        --   cmd = ngls_cmd,
+        --   on_new_config = function(new_config)
+        --     new_config.cmd = ngls_cmd
+        --   end,
+        --   on_attach = on_attach,
+        --   capabilities = capabilities,
+        -- },
 
         lua_ls = {
           -- cmd = {...},
@@ -507,6 +507,38 @@ require('lazy').setup({
           },
         },
       }
+      local ok, mason_registry = pcall(require, 'mason-registry')
+      if not ok then
+        vim.notify 'mason-registry could not be loaded'
+        return
+      end
+
+      local angularls_path =
+      vim.env.HOME .. ".local/share/nvim/mason/packages/angular-language-server/."
+      -- mason_registry.get_package('angular-language-server'):get_install_ath()
+
+      local cmd = {
+        'ngserver',
+        '--stdio',
+        '--tsProbeLocations',
+        table.concat({
+          angularls_path,
+          vim.uv.cwd(),
+        }, ','),
+        '--ngProbeLocations',
+        table.concat({
+          angularls_path .. '/node_modules/@angular/language-server',
+          vim.uv.cwd(),
+        }, ','),
+      }
+
+      local config = {
+        cmd = cmd,
+        on_new_config = function(new_config, new_root_dir)
+          new_config.cmd = cmd
+      end,
+      }
+
 
       require('lspconfig').powershell_es.setup {
         bundle_path = vim.fn.stdpath 'data' .. '/mason/packages/powershell-editor-services/',
