a28ed33591bfea98e42cc93059f88b3fb7bf265b
Joel Holewa <holewa@gmail.com>
2025-05-20 13:38:02 +0200

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -685,7 +685,3 @@ require 'custom.vim-options'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
-require 'custom.vim-options'
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
