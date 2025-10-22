06ffe9935ab36b4f1a3bbbeae4e0c17f9952de95
Joel Holewa <holewa@gmail.com>
2025-10-22 11:29:22 +0200

    Update .config/nvim/lua/keymaps.lua

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -72,7 +72,7 @@ vim.keymap.set('n', '<leader>icl', "iconsole.log('')<ESC>hi")
 vim.keymap.set('n', '<leader>icn', "oconsole.log('')<ESC>hi")
 
 --format windows format to unix
-vim.keymap.set('n', '<Leader>gr.', "mz:%!dos2unix<CR>`z")
+vim.keymap.set('n', '<Leader>gr.', "mz:%!dos2unix<CR>`zzz")
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.keymap.set('n', '<Esc>', ':noh<CR><CR>')
