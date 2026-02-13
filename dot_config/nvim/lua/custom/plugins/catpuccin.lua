3548663519759a194f339cc2b7b47bb0a89f7dc2
Joel Holewa <holewa@gmail.com>
2026-02-13 09:47:35 +0100

    Update .config/nvim/lua/custom/plugins/catpuccin.lua

--- dot_config/nvim/lua/custom/plugins/catpuccin.lua
+++ dot_config/nvim/lua/custom/plugins/catpuccin.lua
@@ -8,7 +8,7 @@ return {
     config = function()
       require('catppuccin').setup {
         flavour = "mocha",
-        -- noice = true,
+        noice = true,
         transparent_background = true,
       }
       vim.cmd.colorscheme 'catppuccin-mocha'
